970963d1192995c6f41409af0f4ce6e99994c9b4
God-father1 <balaharode@gmail.com>
2022-03-17 23:07:24 +0530

    a

---
+++ src/main/java/com/infygo/flight/InfyGoFlight.java
@@ -0,0 +1,14 @@
+package com.infygo.flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class InfyGoFlight {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyGoFlight.class, args);
+	}
+
+}
